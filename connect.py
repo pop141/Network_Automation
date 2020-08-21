60000fb059124bddaf668731cb81a752f3efcf4a
pop141 <62552208+pop141@users.noreply.github.com>
2020-08-21 13:26:02 +0100

    Version 3
    Updated to include debug, and progression bar. Requires input text file of IP address

--- connect.py
+++ connect.py
@@ -4,14 +4,17 @@ import time
 from datetime import date
 from getpass import getpass
 import tempfile
+import PySimpleGUI as sg
 
 
 class Connect:
     def __init__(self):
-        self._default_save = r"Z:\Config BackUp" if os.path.exists(r"Z:\Config BackUp") else tempfile.gettempdir()
+
+        self._default_save = "C:/" if os.path.exists("C:/") else tempfile.gettempdir()
+        self._default_open = "C:/" if os.path.exists("C:/") else tempfile.gettempdir()
         self.today = date.today()
         self.date = self.today.strftime("%d-%b-%Y")
-        self.file_name = ''
+        self.i = 0
 
     @property
     def default_save(self):
@@ -21,6 +24,17 @@ class Connect:
     def default_save(self, value):
         if os.path.exists(value):
             self._default_save = value
+        else:
+            raise ValueError('Path not found')
+
+    @property
+    def default_open(self):
+        return self._default_open
+
+    @default_open.setter
+    def default_open(self, value):
+        if os.path.exists(value):
+            self._default_open = value
         else:
             raise ValueError('Path not found')
 
@@ -35,31 +49,55 @@ class Connect:
             raise ValueError(f"connection {e} please check IPaddress {ip} is avaiable")
         return ssh
 
-    def save_config(self, filename, user, pwd):
+    def save_config(self, filename, save, user, pwd, lines, debug):
         with open(filename, "r") as f0:
-            for ip in f0.readlines():
-                ip = ip.strip()
-                ssh = self.connect_ssh(user, pwd, ip)
+            if debug == False:
+                for ip in f0.readlines():
+                    ip = ip.strip()
+                    ssh = self.connect_ssh(user, pwd, ip)
+                   
+                    sg.OneLineProgressMeter('Device Progress', self.i+1, lines , 'meter')
 
-                if ssh == 0:
-                    continue
+                    terminal = ssh.invoke_shell()
+                    time.sleep(2)
+                    terminal.send('term len 0\n')
+                    time.sleep(1)
+                    terminal.send('write memory\n')
+                    time.sleep(2)
+                    terminal.send('sh run\n')
+                    time.sleep(20)
+                    output = terminal.recv(999999)
+                    os.chdir(save)
+                    file_name = "%s_%s" % (ip, self.date) + ".txt"
+                    with open(file_name, 'a') as backup:
+                        backup.write(output.decode("utf-8"))
+                    
+                    self.i += 1
+                    ssh.close()
 
-                terminal = ssh.invoke_shell()
-                time.sleep(2)
-                terminal.send('term len 0\n')
-                time.sleep(1)
-                print("Saving Config to device")
-                terminal.send('write memory\n')
-                time.sleep(2)
-                print("Config Saved")
-                print(f"backing up config for {ip}")
-                terminal.send('sh run\n')
-                time.sleep(20)
-                output = terminal.recv(999999)
-                file_name = "%s_%s" % (ip, date) + ".txt"
+            elif debug == True:
+                for ip in f0.readlines():
+                    ip = ip.strip()
+                    ssh = self.connect_ssh(user, pwd, ip)
 
-                with open(file_name, 'a') as backup:
-                    backup.write(output.decode("utf-8"))
+                    terminal = ssh.invoke_shell()
+                    time.sleep(2)
+                    terminal.send('term len 0\n')
+                    time.sleep(1)
+                    sg.Print("Saving Config to device")
+                    terminal.send('write memory\n')
+                    time.sleep(2)
+                    sg.Print("Config Saved")
+                    sg.Print(f"backing up config for {ip}")
+                    terminal.send('sh run\n')
+                    time.sleep(20)
+                    output = terminal.recv(999999)
+                    os.chdir(save)
+                    file_name = "%s_%s" % (ip, self.date) + ".txt"
+                    with open(file_name, 'a') as backup:
+                        backup.write(output.decode("utf-8"))
+                    
+                    sg.Print(f"Config saved to {file_name} in location {os.getcwd()} \n")
+                    self.i += 1
+                    ssh.close()
 
-                print(f"Config saved to {file_name} to {os.getcwd()}")
-                ssh.close()
